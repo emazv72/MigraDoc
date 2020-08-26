04d88aa35f6bdba89f708b80a021f60f3db971b4
emazv72 <emanuele.zavallone@gmail.com>
2020-08-26 14:56:18 +0200

    ListInfo fix serialization

--- MigraDoc/src/MigraDoc.DocumentObjectModel/DocumentObjectModel/ListInfo.cs
+++ MigraDoc/src/MigraDoc.DocumentObjectModel/DocumentObjectModel/ListInfo.cs
@@ -113,13 +113,17 @@ namespace MigraDoc.DocumentObjectModel
         internal override void Serialize(XmlSerializer serializer)
         {
 
-            if (!_listType.IsNull)
-                serializer.WriteSimpleAttribute("ListInfo.ListType", ListType);
+			serializer.WriteStartElement("ListInfo");
+
+			if (!_listType.IsNull)
+                serializer.WriteSimpleAttribute("ListType", ListType);
             if (!_numberPosition.IsNull)
-                serializer.WriteSimpleAttribute("ListInfo.NumberPosition", NumberPosition);
+                serializer.WriteSimpleAttribute("NumberPosition", NumberPosition);
             if (!_continuePreviousList.IsNull)
-                serializer.WriteSimpleAttribute("ListInfo.ContinuePreviousList", ContinuePreviousList);
-        }
+                serializer.WriteSimpleAttribute("ContinuePreviousList", ContinuePreviousList);
+
+			serializer.WriteEndElement(); // listinfo
+		}
 
         /// <summary>
         /// Returns the meta object of this instance.
